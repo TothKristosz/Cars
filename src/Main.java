e0e30516cf207410fc2674ff9987549d4af1ffbe
TothKristosz <112863872+TothKristosz@users.noreply.github.com>
2022-11-21 11:11:44 +0100

    disznaj

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Vehicle ujvehicle = new Vehicle();
         Owner ujowner = new Owner();
-    ujvehicle.task1();
+        ujvehicle.task1();
+        ujowner.task1();
     }
 }
