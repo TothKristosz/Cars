03b40724d9cfb1259d3b52a1acc44ad6621740d6
PinterKristosz <112863663+PinterKristosz@users.noreply.github.com>
2022-11-21 11:18:42 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,18 @@ public class Main {
         Vehicle ujvehicle = new Vehicle();
         Owner ujowner = new Owner();
         ujvehicle.task1();
-        ujowner.task1();
+        ujowner.task1();     
+ 
+        Pattern pattern = Pattern.compile("https://www.startr.hu/tudta-e/Amit-a--magyar-forgalmi-rendszamokrol-tudni-erdemes", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Visit https://www.startr.hu/tudta-e/Amit-a--magyar-forgalmi-rendszamokrol-tudni-erdemes!");
+            boolean matchFound = matcher.find();
+    if(matchFound) {
+        System.out.println("Match found");
+    }
+     else {
+        System.out.println("Match not found");
+    }
+  }
+}
     }
 }
